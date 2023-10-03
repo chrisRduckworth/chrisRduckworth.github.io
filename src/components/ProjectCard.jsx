1dab9abdf5e8435be93a0aa1d9ca03e23be1b07c
chris <c_duckworth@hotmail.com>
2023-10-03 17:40:54 +0100

    add isLoading to project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import { projects } from "./projects.json";
 import "../styles/ProjectCard.css";
+import { useState } from "react";
 
 function ProjectCard({ title }) {
   const project = projects.find((project) => project.title === title);
+  const [isLoading, setIsLoading] = useState(true);
   const { images, description } = project;
-  
+
   return (
     <Link to={`/projects/${title}`} className="projectCard">
       <h2>{title}</h2>
-      <img src={`/assets/project-photos/${images[0]}`} alt={`${title} project thumbnail`} className="projectCardThumbnail"/>
+      <div className={isLoading ? "projectCardLoading" : "projectCardLoaded"}>
+        <p className="projectCardLoadingText">Loading...</p>
+      </div>
+      <img
+        src={`/assets/project-photos/${images[0]}`}
+        alt={`${title} project thumbnail`}
+        className={isLoading ? "projectCardLoaded" : "projectCardThumbnail"}
+        onLoad={() => setIsLoading(false)}
+      />
       <p>{description}</p>
     </Link>
   );
