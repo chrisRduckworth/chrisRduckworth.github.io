44e77cf9f3bbfa8a4782562b47acf1e6ec5b20f6
chris <c_duckworth@hotmail.com>
2023-10-03 17:28:52 +0100

    add skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,37 @@
 import { useContext, useEffect } from "react";
 import { HeaderContext } from "../contexts/HeaderContext";
+import { projects } from "./projects.json";
+import SkillCard from "./SkillCard";
+import "../styles/Skills.css";
 
 function Skills() {
   const { setHeader } = useContext(HeaderContext);
+  const skills = projects
+    .map((project) => project.skills)
+    .flat()
+    .filter((skill, i, arr) => arr.indexOf(skill) === i);
 
   useEffect(() => {
     setHeader((currHeader) => {
       return { tab: "Skills", url: "chrisDuckworth > skills.html" };
     });
   }, []);
-  return <p>skills</p>;
+
+  return (
+    <main>
+      <div className="skillsHeader">
+        <h1>Skills</h1>
+        <span>To see projects using a certain skill, click on the logo</span>
+      </div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <div className="skillCardGrid">
+          {skills.map((skill) => (
+            <SkillCard skill={skill} key={skill} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Skills;
