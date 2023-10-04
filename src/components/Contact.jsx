6ab05e349a81c614a1a0b9bf045f27e16d7825c5
chris <c_duckworth@hotmail.com>
2023-10-04 18:44:37 +0100

    add contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { HeaderContext } from "../contexts/HeaderContext";
+import "../styles/Contact.css"
 
 function Contact() {
   const { setHeader } = useContext(HeaderContext);
@@ -9,7 +10,15 @@ function Contact() {
       return { tab: "Contact", url: "chrisDuckworth > contact.html" };
     });
   }, []);
-  return <p>contact</p>;
+  return <main>
+    <h1>Contact</h1>
+    <ul className="contactList">
+      <li>Email: <a href="mailto:c_duckworth@hotmail.com" target="_blank">c_duckworth@hotmail.com</a></li>
+      <li>LinkedIn: <a href="https://www.linkedin.com/in/chris-duckworth-20a9401a3/" target="_blank">chris-duckworth-20a9401a3</a></li>
+      <li>Github: <a href="https://github.com/chrisRduckworth" target="_blank">chrisRduckworth</a></li>
+      <li>CV: <a href="https://docdro.id/JTFoNcM" target="_blank">here</a></li>
+    </ul>
+  </main>
 }
 
 export default Contact;
