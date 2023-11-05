2f6dfd7826286dfc91299e7a1f35e323ac0e420f
chris <c_duckworth@hotmail.com>
2023-11-05 17:00:09 +0000

    fix issue with long names in skillcards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,55 +8,57 @@ import { camelCase } from "../../utils/utils";
 import "../styles/Project.css";
 
 function Project() {
-  const { setHeader } = useContext(HeaderContext);
-  const { title } = useParams();
+	const { setHeader } = useContext(HeaderContext);
+	const { title } = useParams();
 
-  const project = projects.find((project) => project.title === title);
+	const project = projects.find((project) => project.title === title);
 
-  useEffect(() => {
-    window.scrollTo(0, 0)
-    setHeader((currHeader) => {
-      return {
-        tab: "Projects",
-        url: `chrisDuckworth > projects > ${camelCase(title)}.html`,
-      };
-    });
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		setHeader((currHeader) => {
+			return {
+				tab: "Projects",
+				url: `chrisDuckworth > projects > ${camelCase(title)}.html`,
+			};
+		});
+	}, []);
 
-  if (!project)
-    return (
-      <main>
-        <p>Oops, something went wrong</p>
-      </main>
-    );
+	if (!project)
+		return (
+			<main>
+				<p>Oops, something went wrong</p>
+			</main>
+		);
 
-  return (
-    <main>
-      <h1>{project.title}</h1>
-      <ProjectDescription
-        description={project.description}
-        imagesArr={project.images}
-      />
-      <h2>Links</h2>
-      <ul className="projectLinks">
-        {Object.entries(project.links).map(([name, link]) => {
-          return (
-            <li key={name}>
-              <a href={link} target="_blank">
-                {name}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <h2>Skills</h2>
-      <div className="projectSkillsContaner">
-        {project.skills.map((skill) => {
-          return <SkillCard key={skill} skill={skill} />;
-        })}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<h1>{project.title}</h1>
+			<ProjectDescription
+				description={project.description}
+				imagesArr={project.images}
+			/>
+			<h2>Links</h2>
+			<ul className="projectLinks">
+				{Object.entries(project.links).map(([name, link]) => {
+					return (
+						<li key={name}>
+							<a href={link} target="_blank">
+								{name}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+			<h2>Skills</h2>
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<div className="skillCardGrid">
+					{project.skills.map((skill) => {
+						return <SkillCard key={skill} skill={skill} />;
+					})}
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default Project;
