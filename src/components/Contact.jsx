864943f6d0002fd198b812af0e416e15e52f94d4
chris <c_duckworth@hotmail.com>
2024-03-05 14:48:39 +0000

    update CV link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ function Contact() {
       <li>Email: <a href="mailto:c_duckworth@hotmail.com" target="_blank">c_duckworth@hotmail.com</a></li>
       <li>LinkedIn: <a href="https://www.linkedin.com/in/chris-duckworth-20a9401a3/" target="_blank">chris-duckworth-20a9401a3</a></li>
       <li>Github: <a href="https://github.com/chrisRduckworth" target="_blank">chrisRduckworth</a></li>
-      <li>CV: <a href="https://docdro.id/qIIk7Jl" target="_blank">here</a></li>
+      <li>CV: <a href="https://ufile.io/1myjek35" target="_blank">here</a></li>
     </ul>
   </main>
 }
